68856fe65f5cfb5dbd42b0b528a3fafce24f8f2d
hzqhaqhgs <76132927@qq.com>
2017-03-07 11:10:43 +0800

    初始化图完成

---
+++ Code/src/com/cacheserverdeploy/deploy/CostNode.java
@@ -0,0 +1,12 @@
+package com.cacheserverdeploy.deploy;
+/** 
+* @author E-mail: hou76132927@126.com
+* @version Date：2017年3月6日 下午8:43:13 
+* Introduction 
+*/
+public class CostNode {
+	int nodeid;
+	int vexid;
+	int need;
+}
+
